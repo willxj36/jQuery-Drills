0eca5f949837780675e67d0bc41bdffd23cf26e6
willxj36 <willxj36@gmail.com>
2021-01-06 21:23:07 -0700

    working through lab, at task 11

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
 
     $('#textSubmit').keyup(function() {
-        if($('#textSubmit').text().length !== 0) {
-            $('#btnSubmit').removeAttr('disabled');
+        let boxEmpty = false;
+        if($('#textSubmit').val() === '') {
+            boxEmpty = true;
+        };
+        
+        if(boxEmpty) {
+            $('#btnSubmit').attr('disabled', 'disabled');
         } else {
-            $('#btnSubmit').attr('disabled');
-        }
+            $('#btnSubmit').removeAttr('disabled');
+        };
     });
 
-    $('#btnSubmit').click(function(){
+    $('form').after('<div></div>');
+    $('div').css('z-index', '-10');
+    
+    $('#btnSubmit').click(function(event){
+        event.preventDefault();
+        let boxText = $('input').val();
+        //$('div').append($('<h2></h2>').text(boxText));
         alert('You pushed the button!');
         alert($('input').val());
+        /*$('h2').mouseover(function() {
+            $(this).css({
+                'background-color': 'red',
+                'border-radius': '10%'
+            });
+        });*/       //left off here, task 11
     });
+    
 });
